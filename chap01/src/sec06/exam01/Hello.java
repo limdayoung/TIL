1bf5a3946daa9bd4a395faa5df5beb9a25c97639
limdayoung <dlaekdud625@gmail.com>
2018-07-11 23:14:59 +0900

    docs:add file

---
+++ chap01/src/sec06/exam01/Hello.java
@@ -0,0 +1,10 @@
+package sec06.exam01;
+/*
+ * 작성자 : 임다영
+ * 작성일 : 2018.07.11
+ */
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("Hello, welcome to the jave world!");
+	}
+}
